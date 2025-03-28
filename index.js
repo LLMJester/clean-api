a7a97f0365f2d389639e9c0209e124b3308edbf0
LLMJester <136145461+LLMJester@users.noreply.github.com>
2025-03-27 21:40:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const books = [
   { id: 2, title: 'Lord of the Rings' }
 ];
 
+// Root route (this will fix the "Cannot GET /" error)
+app.get('/', (req, res) => {
+  res.send('Welcome to the Book API!');
+});
+
+// Books route
 app.get('/books', (req, res) => {
   res.json(books);
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log('API running on http://localhost:3000');
 });
